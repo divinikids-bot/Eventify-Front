aa036201b1557edf6843ec56e1028fe839de63f3
muhamademil <muhamademil76@gmail.com>
2025-05-04 01:24:47 +0700

    CRUD Events

--- src/utils/useEvent.ts
+++ src/utils/useEvent.ts
@@ -1,6 +1,14 @@
 import { api } from "@/app/lib/axios";
 
 export function useEvent() {
+  interface EventUpdatePayload {
+    title?: string;
+    date?: string;
+    location?: string;
+    description?: string;
+    [key: string]: any; // opsional, kalau ingin lebih fleksibel
+  }
+
   async function createEvent(email: string, password: string) {
     try {
       const response = await api.post("/create-events", { email, password });
@@ -20,10 +28,54 @@ export function useEvent() {
     }
   }
 
-  async function updateEvent(idEvent: number) {}
-  async function deleteEvent(idEvent: number) {}
+  async function updateEvent(eventId: number, updatedData: EventUpdatePayload) {
+    try {
+      const response = await api.put(`/events/${eventId}`, updatedData);
+      return {
+        message: "Update event berhasil",
+        success: true,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: error,
+        success: false,
+      };
+    }
+  }
 
-  async function generateCoupon() {}
+  async function deleteEvent(eventId: number) {
+    try {
+      const response = await api.delete(`/delete-events/${eventId}`);
+      return {
+        message: "Event berhasil dihapus",
+        success: true,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: error,
+        success: false,
+      };
+    }
+  }
+
+  async function generateCoupon(eventId: number) {
+    try {
+      const response = await api.post(`/coupons/generate`, { eventId });
+      return {
+        message: "Kupon berhasil digenerate",
+        success: true,
+        data: response.data.data, // bisa berisi kode kupon
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: error,
+        success: false,
+      };
+    }
+  }
 
   return {
     createEvent,
